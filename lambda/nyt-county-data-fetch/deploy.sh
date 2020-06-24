5393acf10814c5fc918bb2459d55c212968df194
Tom Bedor <tombedor@gmail.com>
2020-06-24 09:41:33 -0700

    update lambda

--- lambda/nyt-county-data-fetch/deploy.sh
+++ lambda/nyt-county-data-fetch/deploy.sh
@@ -6,6 +6,8 @@ LAMBDA_NAME="nyt-county-data-fetch"
 
 rm -f deployable/function.zip
 pip install -r requirements.txt --target ./target
-zip -r9 deployable/function.zip target/*
+cd target
+zip -r9 ../deployable/function.zip .
+cd $OLDPWD
 zip -g deployable/function.zip lambda_function.py
 aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://deployable/function.zip
